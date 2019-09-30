0dccbeb974c5930eb9ed497ec54855a1f4b62c7b
Igor <igor.plahov.a.a@gmail.com>
2019-09-30 16:37:30 +0300

    Command builder for command execution is done

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,58 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+}
+
+String MainWindow::buildCommand(){
+    String res;
+    res = appName + " -title=" + "\"" + songName + "\" " + "-difficulty=" + "\"" + difficuty + "\" ";
+    if (cursors != 0){
+       res += " -cursors=" + std::to_string(cursors);
+    }
+    if (pitch != 0.0){
+       res += " -pitch=" + std::to_string(pitch);
+    }
+    if (speed != 0.0){
+       res += " -speed=" + std::to_string(speed);
+    }
+    if (tag != 0){
+       res += " -tag=" + std::to_string(tag);
+    }
+    if (sittings != 0){
+        res += " -sittings=" + std::to_string(sittings);
+    }
+    if (mover != "flower"){
+        res += " -mover=" + mover;
+    }
+    if (fps){
+        res += " -fps";
+    }
+    if (debug){
+        res += " -debug";
+    }
+
+    return res;
+}
+
+void MainWindow::onAnyClick(){
+    this->difficuty = ui->cSongDifficulty->currentText().toStdString();
+    this->songName = ui->SongSelection->currentText().toStdString();
+    this->mover = ui->mover->currentText().toStdString();
+
+    this->pitch = ui->pitch->value();
+
+    this->speed = ui->speed->value();
+    this->cursors = ui->cursor->text().toInt();
+    this->tag = ui->tag->text().toInt();
+    this->sittings = ui->sittings->text().toInt();
+
+    this->fps = ui->fps->isChecked();
+    this->debug = ui->debug->isChecked();
+
+    QString comm;
+    comm = comm.fromStdString(buildCommand());
+    ui->command->setText(comm);
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +65,68 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_pushButton_clicked()
+{
+    onAnyClick();
+}
+
+void MainWindow::on_SongSelection_currentTextChanged(const QString &arg1)
+{
+    (void)arg1;
+    onAnyClick();
+}
+
+void MainWindow::on_cSongDifficulty_currentTextChanged(const QString &arg1)
+{
+    (void)arg1;
+    onAnyClick();
+}
+
+void MainWindow::on_cursor_valueChanged(const QString &arg1)
+{
+    (void)arg1;
+    onAnyClick();
+}
+
+void MainWindow::on_tag_valueChanged(const QString &arg1)
+{
+    (void)arg1;
+    onAnyClick();
+}
+
+void MainWindow::on_pitch_valueChanged(const QString &arg1)
+{
+    (void)arg1;
+    onAnyClick();
+}
+
+void MainWindow::on_sittings_valueChanged(const QString &arg1)
+{
+    (void)arg1;
+    onAnyClick();
+}
+
+void MainWindow::on_mover_currentTextChanged(const QString &arg1)
+{
+    (void)arg1;
+    onAnyClick();
+}
+
+void MainWindow::on_fps_stateChanged(int arg1)
+{
+    (void)arg1;
+    onAnyClick();
+}
+
+void MainWindow::on_debug_stateChanged(int arg1)
+{
+    (void)arg1;
+    onAnyClick();
+}
+
+void MainWindow::on_speed_valueChanged(const QString &arg1)
+{
+    (void)arg1;
+    onAnyClick();
+}
--- mainwindow.h
+++ mainwindow.h
@@ -1,6 +1,7 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
-
+#include <string>
+typedef  std::string String;
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
@@ -15,7 +16,47 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_pushButton_clicked();
+
+    void on_SongSelection_currentTextChanged(const QString &arg1);
+
+    void on_cSongDifficulty_currentTextChanged(const QString &arg1);
+
+    void on_cursor_valueChanged(const QString &arg1);
+
+    void on_tag_valueChanged(const QString &arg1);
+
+    void on_pitch_valueChanged(const QString &arg1);
+
+    void on_sittings_valueChanged(const QString &arg1);
+
+    void on_mover_currentTextChanged(const QString &arg1);
+
+    void on_fps_stateChanged(int arg1);
+
+    void on_debug_stateChanged(int arg1);
+
+    void on_speed_valueChanged(const QString &arg1);
+
 private:
     Ui::MainWindow *ui;
+    String command;
+    String appName;
+    String songName;
+    String difficuty;
+    String mover;
+
+    int cursors;
+    int tag;
+    double speed;
+    double pitch;
+    int sittings;
+
+    bool fps;
+    bool debug;
+
+    void onAnyClick();
+    String buildCommand();
 };
 #endif // MAINWINDOW_H
